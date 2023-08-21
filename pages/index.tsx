f5a04f57cf280f4763d60ba3fd30a0b0ede2ebec
William McKenzie <williamqmckenzie@gmail.com>
2023-08-21 13:41:04 -0300

    local

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,45 +14,8 @@ const fetcher = (url, data) => {
 };
 
 const TeamTether = () => {
-  const [loginField, setLogin] = useState("")
-  const [passwordField, setPassword] = useState("")
-  const [errorMessage, setError] = useState("")
-  var myClass = "form login__form"
-  var curLogin = ''
-  var curPassword = ''
 
-  const UpdateLogin = event => {
-    curLogin = event.target.value
-    console.log(curLogin)
-    setLogin(curLogin)
-  }
-  const UpdatePassword = event => {
-    curPassword = event.target.value
-    console.log(curPassword)
-    setPassword(curPassword)
-  }
-  const Login = async () => {
-    var res = await fetcher(`http://localhost:3000/api/userconfig?login=${loginField}&password=${passwordField}`, false);
-
-    if (!res.name) setError("User not found")
-    else console.log(res)
-  }
-
-  return (<form className={myClass} id="login">
-    <h1 className="form__title">Index</h1>
-    <div className="form__message form__message--error">{errorMessage}</div>
-    <div className="form__input-group">
-      <input type="text" className="form__input" onChange={UpdateLogin} value={loginField} autoFocus placeholder="Username/email"></input>
-      <div className="form__input-error-message"></div>
-    </div>
-    <div className="form__input-group">
-      <input type="password" className="form__input" onChange={UpdatePassword} value={passwordField} placeholder="Password"></input>
-      <div className="form__input-error-message"></div>
-    </div>
-    <button className="form__button" type="button" onClick={Login}>Submit</button>
-    <p className="form__text">
-      <a href="./" className="form__link">Forgot your password?</a>
-    </p>
+  return (<form>
   </form>
   )
 }
