5a4618d9f341b58d48c9ef4ff8a10f4f2eee6dc5
William McKenzie <williamqmckenzie@gmail.com>
2023-08-22 07:57:04 -0300

    rah

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,10 @@ const TeamTether = () => {
       :
       "ADMIN",
   }
-
+  if (router.query.user) {
+    user = JSON.parse(router.query.user as string)
+  }
+  "if (JSON.parse(router.query.user as string).name == true) user = JSON.parse(router.query.user as string)"
   const [loaded, setLoaded] = useState(false);
   var [popupClass, setPopupClass] = useState("home__popup container")
   var [newIcon, setIcon] = useState(user.icon)
