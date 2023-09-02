628a181b10db531ebb6c7d9fb2ae05fa5ce906cd
William McKenzie <williamqmckenzie@gmail.com>
2023-09-02 16:58:55 -0300

    testing on deployment

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -2,11 +2,28 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
+// export default async function handle(req, res) {
+//     try {
+//         const { login, password } = req.query;
+
+//         const user = await prisma.user.findFirst({
+//             where: {
+//                 OR: [{ name: login }, { email: login }],
+//                 password: password,
+//             },
+//         });
+
+//         res.json(user);
+//     } catch (error) {
+//         res.json("error")
+//     }
+// }
+
 export default async function handle(req, res) {
     try {
         const { login, password } = req.query;
 
-        const user = await prisma.user.findFirst({
+        const user = await prisma.user.findMany({
             where: {
                 OR: [{ name: login }, { email: login }],
                 password: password,
@@ -15,6 +32,6 @@ export default async function handle(req, res) {
 
         res.json(user);
     } catch (error) {
-        res.json("error")
+        res.json(error)
     }
 }
